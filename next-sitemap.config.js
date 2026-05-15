faef522cf89ee26c6e7838afc0c6dd1ba204eb2d
Asim Poudel <bipulp49@gmail.com>
2026-05-15 09:51:49 +0545

    chore: change site name

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://asimreviews.netlify.app", // 🔁 change to your actual domain when you get one
+  siteUrl: process.env.SITE_URL || "https://ranknest.tech", // 🔁 change to your actual domain when you get one
   generateRobotsTxt: true, // (optional)
   sitemapSize: 7000,
   changefreq: 'daily',
