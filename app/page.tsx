bcfd4fccf9533e946edda4607f82a94fa3cac8c3
Asim Poudel <bipulp49@gmail.com>
2026-06-08 12:53:06 +0545

    fix: forgot line

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { getCategoriesToShow } from "@/lib/fetches/categoriesToShow";
 import NewHomePageClient from "./newHomepage/NewHomePageClient";
 
+export const revalidate = 60;
 
 export default async function NewHomePage() {
   const categories = await getCategoriesToShow();
