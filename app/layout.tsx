e13aee421b5345b750b7b171cfe5d61172602388
Asim Poudel <bipulp49@gmail.com>
2026-09-06 17:13:42 +0545

    feat: remove canonical in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,9 +66,6 @@ export const metadata: Metadata = {
     "smart buying advice",
     "consumer electronics reviews",
   ],
-  alternates: {
-    canonical: "/",
-  },
   other: {
     "google-site-verification": "eHK4gnsSuDp43s322_h3gU5BWtKhQBRgj4Bd7wyWT0A",
     "msvalidate.01": "5819ED6C8761B0EFBB070F56184DE371",
